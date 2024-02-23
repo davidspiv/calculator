356a330caf8ad50cad235943643bfad5c52cc130
davidspiv <daspivack@gmail.com>
2024-02-23 13:14:02 -0800

    efficient refactor

--- index.js
+++ index.js
@@ -2,122 +2,69 @@ const cBody = document.querySelector("#c-body");
 const cScreen = document.querySelector("#c-screen");
 const buttonContainer = document.querySelector("#button-container");
 
-const AC = document.querySelector("#AC");
-const signSwitch = document.querySelector("#sign-switch");
-const remainder = document.querySelector("#remainder");
-const divide = document.querySelector("#divide");
-const multiply = document.querySelector("#multiply");
-const subtract = document.querySelector("#subtract");
-const add = document.querySelector("#add");
-const decimal = document.querySelector("#decimal");
-const equals = document.querySelector("#equals");
-
-const value0 = document.querySelector("#value-0");
-
 let returnFlag = false;
-let operatorFlag = false;
 
-value0.addEventListener("click", () => {
-  if (returnFlag) {
-    cScreen.value = "";
-  }
-  const currentValue = cScreen.value;
-  if (currentValue != "") cScreen.value = `${currentValue}${0}`;
-});
+buttonContainer.addEventListener("click", (e) => {
+  const button = e.target.id;
+  const buttonVal = button[button.length - 1];
+  const inputDisplayVal = cScreen.value;
 
-function init() {
-  for (let i = 1; i < 10; i++) {
-    const query = `#value-${i}`;
-    const input = document.querySelector(query);
-    input.addEventListener("click", () => {
-      if (returnFlag === true) {
-        cScreen.value = i;
-        returnFlag = false;
-        operatorFlag = false;
-      } else {
-        const currentValue = cScreen.value;
-        cScreen.value = `${currentValue}${i}`;
-        operatorFlag = false;
-      }
-    });
-  }
-}
-
-init();
-
-AC.addEventListener("click", () => {
-  cScreen.value = "";
-});
-
-signSwitch.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  cScreen.value = currentValue * -1;
-});
-
-remainder.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  if (currentValue != "" && operatorFlag === false) {
-    cScreen.value = `${currentValue}%`;
-    returnFlag = false;
-    operatorFlag = true;
-  }
-});
-
-divide.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  if (currentValue != "" && operatorFlag === false) {
-    cScreen.value = `${currentValue}/`;
-    returnFlag = false;
-    operatorFlag = true;
-  }
-});
-
-multiply.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  if (currentValue != "" && operatorFlag === false) {
-    cScreen.value = `${currentValue}*`;
-    returnFlag = false;
-    operatorFlag = true;
-  }
-});
-
-subtract.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  if (currentValue != "" && operatorFlag === false) {
-    cScreen.value = `${currentValue}-`;
-    returnFlag = false;
-    operatorFlag = true;
-  }
-});
-
-add.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  if (currentValue != "" && operatorFlag === false) {
-    cScreen.value = `${currentValue}+`;
-    returnFlag = false;
-    operatorFlag = true;
-  }
-});
-
-decimal.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  const lastValue = currentValue[currentValue.length - 1];
-  if (currentValue === "") {
-    cScreen.value = `0.`;
-  } else if (lastValue === ".") {
+  if (button === "AC") {
+    cScreen.value = "0";
     return;
-  } else if (operatorFlag === true) {
-    cScreen.value = `${currentValue}0.1`;
-    operatorFlag = false;
-  } else {
-    cScreen.value = `${currentValue}.`;
   }
-});
 
-equals.addEventListener("click", () => {
-  const currentValue = cScreen.value;
-  if (currentValue != "" && isNaN(currentValue)) {
-    cScreen.value = eval(currentValue);
-    returnFlag = true;
+  if (button === "sign-switch") {
+    cScreen.value = inputDisplayVal * -1;
+    return;
   }
+
+  if (button === "equals") {
+    if (inputDisplayVal != "" && isNaN(inputDisplayVal)) {
+      cScreen.value = eval(inputDisplayVal);
+    }
+    returnFlag = true;
+    return;
+  }
+
+  if (button === "decimal") {
+    for (let i = inputDisplayVal.length - 1; i > -1; i--) {
+      const digit = inputDisplayVal[i];
+      if (isNaN(digit)) {
+        if (digit === ".") {
+          return;
+        }
+        if (digit != "0") {
+          break;
+        }
+      }
+    }
+    cScreen.value = `${inputDisplayVal}.`;
+    returnFlag = false;
+    return;
+  }
+
+  //BUTTONS [0-9]
+  if (!isNaN(buttonVal)) {
+    if (buttonVal === "0") {
+      if (returnFlag) {
+        cScreen.value = "";
+      }
+      if (inputDisplayVal != "") cScreen.value = `${inputDisplayVal}${0}`;
+      return;
+    } else {
+      if (returnFlag === true || inputDisplayVal === "0") {
+        cScreen.value = buttonVal;
+        returnFlag = false;
+      } else {
+        const inputDisplayVal = cScreen.value;
+        cScreen.value = `${inputDisplayVal}${buttonVal}`;
+      }
+    }
+    return;
+  }
+
+  //OPERATORS [%, /, *, -, +]
+  cScreen.value = `${inputDisplayVal}${buttonVal}`;
+  returnFlag = false;
 });
