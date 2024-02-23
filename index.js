65756ff672e4e30d11c23fe46e5de5edf6119af6
davidspiv <daspivack@gmail.com>
2024-02-22 22:51:24 -0800

    working except for decimal

--- index.js
+++ index.js
@@ -6,18 +6,118 @@ const AC = document.querySelector("#AC");
 const signSwitch = document.querySelector("#sign-switch");
 const remainder = document.querySelector("#remainder");
 const divide = document.querySelector("#divide");
-const value7 = document.querySelector("#value-7");
-const value8 = document.querySelector("#value-8");
-const value9 = document.querySelector("#value-9");
 const multiply = document.querySelector("#multiply");
-const value4 = document.querySelector("#value-4");
-const value5 = document.querySelector("#value-5");
-const value6 = document.querySelector("#value-6");
 const subtract = document.querySelector("#subtract");
-const value1 = document.querySelector("#value-1");
-const value2 = document.querySelector("#value-2");
-const value3 = document.querySelector("#value-3");
 const add = document.querySelector("#add");
-const value0 = document.querySelector("#value-0");
 const decimal = document.querySelector("#decimal");
 const equals = document.querySelector("#equals");
+
+const value0 = document.querySelector("#value-0");
+
+let returnFlag = false;
+let operatorFlag = false;
+
+value0.addEventListener("click", () => {
+  if (returnFlag) {
+    cScreen.value = "";
+  }
+  const currentValue = cScreen.value;
+  if (currentValue != "") cScreen.value = `${currentValue}${0}`;
+});
+
+function init() {
+  for (let i = 1; i < 10; i++) {
+    const query = `#value-${i}`;
+    const input = document.querySelector(query);
+    input.addEventListener("click", () => {
+      if (returnFlag === true) {
+        cScreen.value = i;
+        returnFlag = false;
+        operatorFlag = false;
+      } else {
+        const currentValue = cScreen.value;
+        cScreen.value = `${currentValue}${i}`;
+        operatorFlag = false;
+      }
+    });
+  }
+}
+
+init();
+
+AC.addEventListener("click", () => {
+  cScreen.value = "";
+});
+
+signSwitch.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  cScreen.value = currentValue * -1;
+});
+
+remainder.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  if (currentValue != "" && operatorFlag === false) {
+    cScreen.value = `${currentValue}%`;
+    returnFlag = false;
+    operatorFlag = true;
+  }
+});
+
+divide.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  if (currentValue != "" && operatorFlag === false) {
+    cScreen.value = `${currentValue}/`;
+    returnFlag = false;
+    operatorFlag = true;
+  }
+});
+
+multiply.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  if (currentValue != "" && operatorFlag === false) {
+    cScreen.value = `${currentValue}*`;
+    returnFlag = false;
+    operatorFlag = true;
+  }
+});
+
+subtract.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  if (currentValue != "" && operatorFlag === false) {
+    cScreen.value = `${currentValue}-`;
+    returnFlag = false;
+    operatorFlag = true;
+  }
+});
+
+add.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  if (currentValue != "" && operatorFlag === false) {
+    cScreen.value = `${currentValue}+`;
+    returnFlag = false;
+    operatorFlag = true;
+  }
+});
+
+decimal.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  const lastValue = currentValue[currentValue.length - 1];
+  if (currentValue === "") {
+    cScreen.value = `0.`;
+  } else if (lastValue === ".") {
+    return;
+  } else if (operatorFlag === true) {
+    cScreen.value = `${currentValue}0.1`;
+    operatorFlag = false;
+  } else {
+    cScreen.value = `${currentValue}.`;
+  }
+});
+
+equals.addEventListener("click", () => {
+  const currentValue = cScreen.value;
+  if (currentValue != "" && isNaN(currentValue)) {
+    cScreen.value = eval(currentValue);
+    returnFlag = true;
+  }
+});
